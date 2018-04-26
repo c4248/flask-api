7d1a80edeaf5718deb0fb3d485027a6d1f83fc01
francis <c4248@outlook.com>
2018-04-25 20:54:42 -0700

    flask api with sqlite database

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required
 from security import authenticate, identity
+from user import UserRegister
 
 app = Flask(__name__)
 app.secret_key = 'jose'
@@ -51,8 +52,17 @@ class Item(Resource):
             item.update(data)
         return item
 
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', 'http://localhost:3000')
+    if request.method == 'OPTIONS':
+        response.headers['Access-Control-Allow-Methods'] = 'DELETE, GET, POST, PUT'
+        headers = request.headers.get('Access-Control-Request-Headers')
+        if headers:
+            response.headers['Access-Control-Allow-Headers'] = headers
+    return response
 
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
-
+api.add_resource(UserRegister, '/register')
 app.run(debug=True, port=5000)
